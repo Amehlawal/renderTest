e379cb864f4ffc974e69e1ea4244d3f4e5c64867
Lawal Ameh <amehlawal@gmail.com>
2025-04-15 20:00:54 +0100

    added static root dir

--- hotel/settings.py
+++ hotel/settings.py
@@ -168,6 +168,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+# For collectstatic (used in production)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
